2c1ce8b03a3c599ed780700668f687422e481917
refefer <astanton@theatlantic.com>
2012-09-11 19:47:44 -0400

    Adds piechart viewer.

--- priv/static/js/pulsar.utils.js
+++ priv/static/js/pulsar.utils.js
@@ -21,6 +21,39 @@ var PulsarUtils = (function() {
                 d[keyvalue[0]] = values.length === 1 ? values[0] : values;
             }
             return d;
+        },
+
+        sort_data: function(data) {
+            return data.sort(function(a, b) {
+                if(a[0] < b[0]) {
+                    return -1;
+                } else if(a[0] > b[0]) {
+                    return 1
+                }
+                return 0;
+            });           
+        },
+
+        data_to_percent: function(data, places) {
+            places = typeof places  === "integer" ? places : 2;
+
+            // Count up everything
+            var total = 0;
+            for(var i = 0; i < data.length; i++) {
+                total += data[i][1];
+            }
+
+            data = data.slice();
+
+            // Normalize 
+            var mag = Math.pow(10, places);
+            for(var i = 0; i < data.length; i++) {
+                var d = data[i][1] / total;
+                data[i][1] = Math.round(mag * d) / mag;
+            }
+
+            return data;
         }
+
     }
 })();
