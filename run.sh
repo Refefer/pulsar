0a224166e924bcb26487bf2d95f99051d9c82698
refefer <astanton@theatlantic.com>
2012-09-10 20:13:23 -0400

    config now in conf/sys.config, adds run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rebar compile && erl -boot start_sasl -sname pulsar -pa deps/*/ebin ebin -s pulsar +A 4 +K true
+rebar compile && erl -boot start_sasl -sname pulsar -pa deps/*/ebin ebin -s pulsar +A 4 +K true -config conf/sys.config
